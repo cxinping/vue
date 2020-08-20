b8e69473b85e807f7db98093caf4207160da1a84
cxinping <xpws2006@163.com>
2020-08-20 22:57:55 +0800

    ***************   系统初始化 ***********************

--- Chapter01/RuoYi-Vue/ruoyi-ui/src/layout/components/Sidebar/Logo.vue
+++ Chapter01/RuoYi-Vue/ruoyi-ui/src/layout/components/Sidebar/Logo.vue
@@ -26,7 +26,7 @@ export default {
   },
   data() {
     return {
-      title: '若依管理系统',
+      title: '伽罗生活管理系统',
       logo: logoImg
     }
   }
--- Chapter01/RuoYi-Vue/sql/jalo.sql
+++ Chapter01/RuoYi-Vue/sql/jalo.sql
@@ -53,7 +53,7 @@ CREATE TABLE `jalo_loan_usage_table` (
 ) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COMMENT='贷款使用表';
 
 -- ----------------------------
--- 创建库存表表的视图
+-- 创建库存表的视图
 -- ----------------------------
 CREATE VIEW jalo_inventory_view AS SELECT
 	DATE_FORMAT(create_time, '%Y-%m-%d') AS days,
@@ -61,11 +61,116 @@ CREATE VIEW jalo_inventory_view AS SELECT
 	SUM(unsaleable_inventory_amount	) AS sum_unsaleable_inventory_amount,
 	SUM(inventory_amount) AS sum_inventory_amount
 FROM
-	jalo_inventory_sum
+	jalo_inventory_detail
 GROUP BY
 	days
 ORDER BY
 	create_time DESC
 
+-- ----------------------------
+-- 创建采购订单跟踪信息表的视图
+-- ----------------------------
+CREATE VIEW jalo_purchase_order_tracking_information_view AS
+SELECT
+	DATE_FORMAT(create_time, '%Y-%m-%d') AS days,
+	SUM(prepayment_amount_paid) AS sum_prepayment_amount_paid
+FROM
+	jalo_purchase_order_tracking_information
+GROUP BY
+	days
+ORDER BY
+	create_time DESC
 
+-- ----------------------------
+-- 创建已发货未结算金额表的视图
+-- ----------------------------
+CREATE VIEW jalo_shipped_unsettled_view AS
+SELECT
+	DATE_FORMAT(create_time, '%Y-%m-%d') AS days,
+	SUM(total_amount) AS sum_shipped_unsettled_total_amount
+FROM
+	jalo_shipped_unsettled
+GROUP BY
+	days
+ORDER BY
+	create_time DESC
+
+-- ----------------------------
+-- 创建可用资金表的视图
+-- ----------------------------
+CREATE VIEW jalo_available_funds_view AS
+SELECT
+	DATE_FORMAT(create_time, '%Y-%m-%d') AS days,
+	SUM(total_amount) AS sum_available_funds_total_amount
+FROM
+	jalo_available_funds
+GROUP BY
+	days
+ORDER BY
+	create_time DESC
+
+-- ----------------------------
+-- 创建在途物资表的视图
+-- ----------------------------
+CREATE VIEW jalo_goods_transit_view AS
+SELECT
+	DATE_FORMAT(create_time, '%Y-%m-%d') AS days,
+	SUM(total_amount) AS goods_transit_total_amount
+FROM
+	jalo_goods_transit
+GROUP BY
+	days
+ORDER BY
+	create_time DESC
+
+-- ----------------------------
+-- 创建已下单未发货表的视图
+-- ----------------------------
+CREATE VIEW jalo_placed_not_shipped_view AS
+SELECT
+	DATE_FORMAT(create_time, '%Y-%m-%d') AS days,
+	SUM(placed_not_shipped) AS placed_not_shipped_total_amount
+FROM
+	jalo_placed_not_shipped
+GROUP BY
+	days
+ORDER BY
+	create_time DESC
+
+-- ----------------------------
+-- 创建统计页面表的视图
+-- ----------------------------
+CREATE VIEW jalo_amount_statistics_view AS
+SELECT
+	inventory.days,
+	inventory.sum_saleable_inventory_amount,
+	inventory.sum_unsaleable_inventory_amount,
+	inventory.sum_inventory_amount,
+	information.sum_prepayment_amount_paid
+FROM
+	jalo_inventory_view AS inventory
+LEFT JOIN jalo_purchase_order_tracking_information_view AS information ON inventory.days = information.days
+ORDER BY
+	inventory.days DESC
+
+CREATE VIEW jalo_amount_statistics_view AS
+SELECT
+	inventory.days,
+	inventory.sum_saleable_inventory_amount,
+	inventory.sum_unsaleable_inventory_amount,
+	inventory.sum_inventory_amount,
+	information.sum_prepayment_amount_paid,
+	unsettled.sum_shipped_unsettled_total_amount,
+	funds.sum_available_funds_total_amount,
+    transit.goods_transit_total_amount,
+	shipped.placed_not_shipped_total_amount
+FROM
+	jalo_inventory_view AS inventory
+LEFT JOIN jalo_purchase_order_tracking_information_view AS information ON inventory.days = information.days
+LEFT JOIN jalo_shipped_unsettled_view AS unsettled ON inventory.days = unsettled.days
+LEFT JOIN jalo_available_funds_view AS funds ON inventory.days = funds.days
+LEFT JOIN jalo_goods_transit_view AS transit ON inventory.days = transit.days
+LEFT JOIN jalo_placed_not_shipped_view as shipped ON inventory.days = shipped.days
+ORDER BY
+	inventory.days DESC
 
