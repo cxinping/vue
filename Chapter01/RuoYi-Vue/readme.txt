fdc9c495d2c46be68e56213b182f33612c62a131
cxinping <xpws2006@163.com>
2020-08-30 17:53:21 +0800

    优化代码

--- Chapter01/RuoYi-Vue/readme.txt
+++ Chapter01/RuoYi-Vue/readme.txt
@@ -39,6 +39,9 @@ uuid
 
 Authorization:eyJhbGciOiJIUzUxMiJ9.eyJsb2dpbl91c2VyX2tleSI6ImFlMzYxMjAzLTJlOWQtNDE0Yi04NWQzLTAyNmNjZGZiMjY3MSJ9.rod4cZpJGzN1dPfl_seI3P_cuaM71hOq_uDh3I30rcEJX6gWOL4nYPuv22Xhv5LdmovKyEMcUIifE_ZM_GRybw
 
+部署项目
+nohup jva -jar ruoyi-admin.jar &
+
 
 http://www.ultrapower.com:8888
 admin
---
+++ Chapter01/RuoYi-Vue/sql/menu.sql
@@ -0,0 +1,8 @@
+
+
+INSERT INTO `sys_menu` VALUES ('2049', '已下单未发货', '2000', '8', 'shipped', 'amount/shipped/index', '1', 'C', '0', '0', 'amount:shipped:list', 'dict', 'admin', '2018-03-01 00:00:00', 'admin', '2020-08-30 12:38:12', '已下单未发货菜单');
+INSERT INTO `sys_menu` VALUES ('2050', '已下单未发货查询', '2049', '1', '#', '', '1', 'F', '0', '0', 'amount:shipped:query', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2020-08-19 18:56:46', '');
+INSERT INTO `sys_menu` VALUES ('2051', '已下单未发货新增', '2049', '2', '#', '', '1', 'F', '0', '0', 'amount:shipped:add', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2020-08-19 18:56:52', '');
+INSERT INTO `sys_menu` VALUES ('2052', '已下单未发货修改', '2049', '3', '#', '', '1', 'F', '0', '0', 'amount:shipped:edit', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2020-08-19 18:56:58', '');
+INSERT INTO `sys_menu` VALUES ('2053', '已下单未发货删除', '2049', '4', '#', '', '1', 'F', '0', '0', 'amount:shipped:remove', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2020-08-19 18:57:06', '');
+INSERT INTO `sys_menu` VALUES ('2054', '已下单未发货导出', '2049', '5', '#', '', '1', 'F', '0', '0', 'amount:shipped:export', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2020-08-19 18:57:30', '');
