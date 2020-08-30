4311922e8648d3ef8bacafd3050a5844fb6c3e7e
cxinping <xpws2006@163.com>
2020-08-30 17:53:34 +0800

    优化代码

--- Chapter01/RuoYi-Vue/sql/jalo.sql
+++ Chapter01/RuoYi-Vue/sql/jalo.sql
@@ -57,9 +57,9 @@ CREATE TABLE `jalo_loan_usage_table` (
 -- ----------------------------
 CREATE VIEW jalo_inventory_view AS SELECT
 	DATE_FORMAT(create_time, '%Y-%m-%d') AS days,
-	SUM(saleable_inventory_amount) AS sum_saleable_inventory_amount,
-	SUM(unsaleable_inventory_amount	) AS sum_unsaleable_inventory_amount,
-	SUM(inventory_amount) AS sum_inventory_amount
+	SUM(saleable_inventory_amount) AS sum_saleable_inventory_amount,      -- 可销售库存金额
+	SUM(unsaleable_inventory_amount	) AS sum_unsaleable_inventory_amount, -- 不可销售库存金额
+	SUM(inventory_amount) AS sum_inventory_amount     -- 合计金额
 FROM
 	jalo_inventory_detail
 GROUP BY
@@ -73,7 +73,9 @@ ORDER BY
 CREATE VIEW jalo_purchase_order_tracking_information_view AS
 SELECT
 	DATE_FORMAT(create_time, '%Y-%m-%d') AS days,
-	SUM(prepayment_amount_paid) AS sum_prepayment_amount_paid
+	SUM(contract_amount) as sum_contract_amount,   -- 合同金额
+    SUM(prepayment_amount_payable) AS sum_prepayment_amount_payable,   -- 应付预付款金额
+	SUM(prepayment_amount_paid) AS sum_prepayment_amount_paid   -- 应付尾款金额
 FROM
 	jalo_purchase_order_tracking_information
 GROUP BY
@@ -90,6 +92,20 @@ SELECT
 	SUM(total_amount) AS sum_shipped_unsettled_total_amount
 FROM
 	jalo_shipped_unsettled
+GROUP BY
+	days
+ORDER BY
+	create_time DESC
+
+-- ----------------------------
+-- 创建已下单未发货金额表的视图
+-- ----------------------------
+CREATE VIEW jalo_purchased_unshipped_view AS
+SELECT
+	DATE_FORMAT(create_time, '%Y-%m-%d') AS days,
+	SUM(total_amount) AS sum_purchased_unshipped_total_amount
+FROM
+	jalo_purchased_unshipped
 GROUP BY
 	days
 ORDER BY
@@ -118,6 +134,22 @@ SELECT
 	SUM(total_amount) AS goods_transit_total_amount
 FROM
 	jalo_goods_transit
+GROUP BY
+	days
+ORDER BY
+	create_time DESC
+
+-- ----------------------------
+-- 创建贷款使用表的视图
+-- ----------------------------
+CREATE VIEW jalo_loan_usage_view AS
+SELECT
+	DATE_FORMAT(create_time, '%Y-%m-%d') AS days,
+	SUM(loan_credit_amount) AS loan_usage_total_loan_credit_amount,
+    SUM(loan_credit_balance) AS loan_usage_total_loan_credit_balance,
+    SUM(loan_credit_remaining) AS loan_usage_total_loan_credit_remaining
+FROM
+	jalo_loan_usage
 GROUP BY
 	days
 ORDER BY
@@ -153,24 +185,35 @@ LEFT JOIN jalo_purchase_order_tracking_information_view AS information ON invent
 ORDER BY
 	inventory.days DESC
 
+
+DROP VIEW jalo_amount_statistics_view;
+
 CREATE VIEW jalo_amount_statistics_view AS
 SELECT
 	inventory.days,
-	inventory.sum_saleable_inventory_amount,
-	inventory.sum_unsaleable_inventory_amount,
-	inventory.sum_inventory_amount,
-	information.sum_prepayment_amount_paid,
-	unsettled.sum_shipped_unsettled_total_amount,
-	funds.sum_available_funds_total_amount,
-    transit.goods_transit_total_amount,
-	shipped.placed_not_shipped_total_amount
+    information.sum_contract_amount ,                -- 采购合同款， 合同金额
+    information.sum_prepayment_amount_payable,       -- 已付预付款， 应付预付款金额
+	information.sum_prepayment_amount_paid,          -- 应付尾款， 应付尾款金额
+	unsettled.sum_shipped_unsettled_total_amount,    -- 已发货未结算
+     purchased.sum_purchased_unshipped_total_amount, -- 已下单未发货
+	loan.loan_usage_total_loan_credit_amount,        -- 贷款授信额度， 贷款授信金额
+	loan.loan_usage_total_loan_credit_balance,       -- 贷款已使用金额， 贷款使用金额
+	loan.loan_usage_total_loan_credit_remaining,     -- 剩余可使用额度， 剩余可使用额度
+	funds.sum_available_funds_total_amount,          --  可用资金
+	inventory.sum_inventory_amount,                  -- 库存
+	inventory.sum_saleable_inventory_amount,         -- 可销售库存,
+	inventory.sum_unsaleable_inventory_amount,       -- 等通知发货库存
+    transit.goods_transit_total_amount               -- 在途物资
+
 FROM
 	jalo_inventory_view AS inventory
 LEFT JOIN jalo_purchase_order_tracking_information_view AS information ON inventory.days = information.days
 LEFT JOIN jalo_shipped_unsettled_view AS unsettled ON inventory.days = unsettled.days
+LEFT JOIN jalo_purchased_unshipped_view as purchased ON inventory.days = purchased.days
+LEFT JOIN jalo_loan_usage_view as loan ON inventory.days = loan.days
 LEFT JOIN jalo_available_funds_view AS funds ON inventory.days = funds.days
 LEFT JOIN jalo_goods_transit_view AS transit ON inventory.days = transit.days
-LEFT JOIN jalo_placed_not_shipped_view as shipped ON inventory.days = shipped.days
+
 ORDER BY
-	inventory.days DESC
+	inventory.days DESC;
 
